a9d0270b1387bcae29dd436a77a30be5cdef1de0
l3oss123456 <46367212+l3oss123456@users.noreply.github.com>
2020-09-30 21:07:38 +0700

    add feature routes

---
+++ src/cores/routes/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "../../containers/home/index";
+
+const Routes = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+      </Switch>
+    </Router>
+  );
+};
+export default Routes;
